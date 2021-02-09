4d1eef3d3e7f506c7acbc50cf2c2d94da6ae1fc1
Azzryel <AzryelShroud@gmail.com>
2021-02-09 16:23:24 +0100

    create basic route template

---
+++ src/components/Shop.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+const Shop = () => {
+
+    return (
+        <div>
+            <h1>Shop</h1>
+        </div>
+    );
+};
+
+export default Shop;
