5da8472f39337f12bb2c2e045c2352119210f49b
Azzryel <AzryelShroud@gmail.com>
2021-02-09 18:38:11 +0100

    add uniqid

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import items from "./items";
+import uniqid from "uniqid";
 
 const Shop = () => {
     const [ itemList, setItemList ] = useState(items);
@@ -12,10 +13,10 @@ const Shop = () => {
             <div className="shop-container">
             {itemList.map( item => {
                return( 
-                    <div className="item-container">
-                        <h5>{item.name}</h5>
-                        <img src={item.img} />
-                        <p> {item.money} &#164; </p>
+                    <div key={uniqid()} className="item-container">
+                        <h5 key={uniqid()}>{item.name}</h5>
+                        <img key={uniqid()} src={item.img} />
+                        <p key={uniqid()}> {item.money} &#164; </p>
                     </div>
                );
             }) 
