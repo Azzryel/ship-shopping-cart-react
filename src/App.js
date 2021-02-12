cc312e402d4e1469459a11cc71d982c117d58b40
Azzryel <AzryelShroud@gmail.com>
2021-02-12 17:26:30 +0100

    style home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import "./styles/app.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1> Home </h1>
+    <div className="App" >
+      <h1> Welcome to Jolly Roger </h1>
+
+      <div className="container">
+        <p> Get your own ship and set sails to explore the world and discover treasures!</p>
+        <Link to="/shop">
+          <button> Check it out </button>
+        </Link>
+      </div>
 
     </div>
   );
