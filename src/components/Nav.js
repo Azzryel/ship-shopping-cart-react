064dfa901c32ed0fb2e0a3b3e76d552603225250
Azzryel <AzryelShroud@gmail.com>
2021-02-12 14:51:47 +0100

    show number of different items next to Cart, update on location change

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation, withRouter } from "react-router-dom";
 import "../styles/nav.css";
+import { myCart } from "./myCart";
 
 const Nav = () => {
+
+    const [ inCart, setInCart ] = useState(myCart.length);
+    const location = useLocation();
+
+    useEffect(() => {
+        console.log("Hey")
+        update();
+    }, [location]);
+
+
+    function update() {
+        setInCart(myCart.length);
+    }
 
     return (
         <nav>
             <h5> Jolly Roger! </h5>
             <div className="link-container"> 
-            <Link to="/">
+            <Link to="/"  >
                 <span>Home</span>
             </Link>
-            <Link to="/shop">
+            <Link to="/shop"  >
                 <span>Shop</span>
             </Link>
-            <Link to="/cart">
-                <span>Cart</span>
+            <Link to="/cart"  >
+                <span>Cart { (inCart>0) ? (
+                    <em>({inCart}) </em>
+                ) : (
+                    ""
+                ) } </span>
             </Link>
             </div>
         </nav>
     )
 };
 
-export default Nav;
+export default withRouter(Nav);
